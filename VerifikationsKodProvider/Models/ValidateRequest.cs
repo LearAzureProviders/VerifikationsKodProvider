edc18e72438f640ce71c215c429a977ab47d747c
lear23 <130910243+lear23@users.noreply.github.com>
2024-07-09 16:18:30 +0200

    Fixade verification code och provade i Postman

---
+++ VerifikationsKodProvider/Models/ValidateRequest.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace VerifikationsKodProvider.Models;
+
+public class ValidateRequest
+{
+    public string Email { get; set; } = null!;
+    public string Code { get; set; } = null!;
+
+}
